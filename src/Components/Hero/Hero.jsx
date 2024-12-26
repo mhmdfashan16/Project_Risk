aff17ead4361d1264037516991f5649c2d465af2
Musharrafmrm <musharrafmrm27@gmail.com>
2024-12-26 23:03:57 +0530

    update hero

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -5,15 +5,15 @@ import left_arrow from '../Things/dark-arrow.png'
 const Hero = () => {
   return (
     <div className='hero container'>
-        <div className="hero_text">
-        <h1 className='hero_title'>Welcome to Commerce World</h1>
-            <h1>We Ensure Better Education for Better World</h1>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur amet obcaecati a deleniti asperiores earum eos, ad vitae commodi beatae dignissimos sint incidunt consequuntur ab. Dolore voluptatem repellat voluptatibus quae?
-            </p>
-            <button className='btn'>Explore more <img src={left_arrow}/></button>
-        </div>
-      
+      <div className="hero_text">
+        <h1 className='hero_title'>Welcome to Commerce World College</h1>
+        <h2>We Ensure Better Education for Better World</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur amet obcaecati a deleniti asperiores earum eos, ad vitae commodi beatae dignissimos sint incidunt consequuntur ab. Dolore voluptatem repellat voluptatibus quae?
+        </p>
+        <button className='btn'>Explore more <img src={left_arrow} /></button>
+      </div>
+
     </div>
   )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,23 +2,24 @@ import React, { useEffect, useState } from 'react';
 import './Navbar.css'
 import logo from '../Things/logo.png'
 import Com_Logo from '../Things/Asset/Com_Logo.png'
+import { Link, ScrollLink } from 'react-scroll';
 
 // import logo from './assets/logo.png';
 
 
 const Navbar = () => {
 
-  const [sticky, setSticky]=useState(false);
-  useEffect(()=>{
-    window.addEventListener('scroll',()=>{
-      window.scrollY > 500?setSticky(true):setSticky(false)
+  const [sticky, setSticky] = useState(false);
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      window.scrollY > 500 ? setSticky(true) : setSticky(false)
     })
-  },[])
+  }, [])
 
   return (
-    <nav className={`container ${sticky? 'dark_nav':''}`}>
-      <img className='navbar_logo' src={Com_Logo}/>
-      
+    <nav className={`container ${sticky ? 'dark_nav' : ''}`}>
+      <img className='navbar_logo' src={Com_Logo} />
+
       {/* <ul className='navbar_menu'>
         <li><Link to='Hero' smooth={true} offset={0} duration={500}>Home</Link></li>
         <li><Link to='Program' smooth={true} offset={0} duration={500}>Program</Link></li>
@@ -26,9 +27,9 @@ const Navbar = () => {
         <li><Link to='Campus' smooth={true} offset={0} duration={500}>Campus</Link></li>
         <li><Link to='Testimonial' smooth={true} offset={0} duration={500}>Testimonias</Link></li>
         <li><Link to='contact' smooth={true} offset={0} duration={500} className='btn'>Contact us</Link></li>
-      </ul> */}
+      </ul > */}
 
-      
+
 
       <ul className='navbar_menu'>
         <li>Home</li>
@@ -38,7 +39,7 @@ const Navbar = () => {
         <li>Testimonias</li>
         <li><button className='btn'>Contact us</button></li>
       </ul>
-    </nav>
+    </nav >
 
   )
 }
